72b3263067501ecd23ae2527b653d60143fce2ce
Alan Aguilar <93650566+tiltedveggie@users.noreply.github.com>
2022-08-28 03:13:00 -0300

    Add files via upload

--- logic.js
+++ logic.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+	$('a, img').attr('draggable', 'false');
+
 	$(".hamburger").click(function() {
 		$(".stick1").toggleClass("selected")
 		$(".stick2").toggleClass("selected2")
@@ -7,8 +9,12 @@ $(document).ready(function() {
 		$(".nav_list_link").toggleClass("selected4")
 	})
 
-	const year = new Date()
+	$('.contact_box_action').hover(function() {
+		$(this).siblings('.cicon').toggleClass('onHover')
+	})
 
-	document.getElementById("year").innerHTML = year.getFullYear()
+	const year = new Date();
+
+	document.getElementById("year").innerHTML = year.getFullYear();
 
 })
